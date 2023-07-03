0fd3f939177c09d5ab370a3bd446ffb6141d48bb
Ankita Bhagwat Sapnar <138100018+AnkitaS210@users.noreply.github.com>
2023-07-03 23:15:13 +0530

    PDFMerger.py

--- PDFMerger.py
+++ PDFMerger.py
@@ -1,7 +1,4 @@
-# PROJECT 04 : PDF MERGER
-
-# modules to be installed:
-# 1. pip install pypdf2 (this is installing name) and import as "PyPDF2"
+# PDF MERGER
 
 import PyPDF2
 # with this module we can do slicing,merging of PDFs
@@ -14,5 +11,3 @@ for filename in pdfiles:
         merger.append(pdfReader)
 pdfFile.close()
 merger.write('merged.pdf')
-
-#here the two pdfs "1.pdf" and "2.pdf" have been successfully merged i.e. "merged.pdf" ; we can check it in the 5 projects section
